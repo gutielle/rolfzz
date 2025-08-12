b2132de2fd3ac4c7c67e8bce713de1a4eda8429e
gutielle <zgutielle@gmail.com>
2025-08-12 20:04:22 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -35,16 +35,17 @@ export default defineConfig({
         theme_color: '#F3F30C',
         background_color: '#0A0A0A',
         display: 'standalone',
-        start_url: '.',
+        start_url: './',
+        scope: './',
         icons: [
           {
-            src: 'icon-192x192.png',
+            src: './icon-192x192.png',
             sizes: '192x192',
             type: 'image/png',
             purpose: 'any maskable'
           },
           {
-            src: 'icon-512x512.png',
+            src: './icon-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
